065b71f1cb89db21c0377a73804c3d02093950fb
Archimedes Trajano <arch@trajano.net>
2010-08-01 18:21:59 -0400

    updated settings to format to trajano.net standards also updated the applicationContext.xml to inline the bean

--- src/main/java/org/example/helloservice/impl/HelloImpl.java
+++ src/main/java/org/example/helloservice/impl/HelloImpl.java
@@ -1,12 +1,9 @@
 package org.example.helloservice.impl;
 
-import javax.jws.WebService;
-
 import net.trajano.ws.helloservice.Hello;
 import net.trajano.ws.helloservice.SayHello;
 import net.trajano.ws.helloservice.SayHelloResponse;
 
-@WebService()
 public class HelloImpl implements Hello {
 
 	private String helloString;
@@ -15,18 +12,18 @@ public class HelloImpl implements Hello {
 		return helloString;
 	}
 
+	@Override
+	public SayHelloResponse sayHello(final SayHello parameters) {
+		System.out.println("got " + parameters.getIn() + " for object "
+				+ hashCode());
+		final SayHelloResponse r = new SayHelloResponse();
+		r.setOut("{{" + getHelloString() + parameters.getIn());
+		return r;
+	}
+
 	public void setHelloString(final String helloString) {
 		System.out.println("INJECTION INVOKED " + helloString);
 		this.helloString = helloString;
-	}
-
-	@Override
-	public SayHelloResponse sayHello(SayHello parameters) {
-		System.out.println("got " + parameters.getIn() + " for object "
-				+ this.hashCode());
-		SayHelloResponse r = new SayHelloResponse();
-		r.setOut("{{" + getHelloString() + parameters.getIn());
-		return r;
 	}
 
 }
