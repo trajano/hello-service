f051184de3ca66410a2068afc25c62fe1400979a
Archimedes Trajano <arch@trajano.net>
2010-08-01 19:50:15 -0400

    Added sanity test to check the hello world

--- src/test/java/net/trajano/ws/helloservice/test/SanityTest.java
+++ src/test/java/net/trajano/ws/helloservice/test/SanityTest.java
@@ -1,9 +1,28 @@
 package net.trajano.ws.helloservice.test;
 
+import net.trajano.ws.helloservice.Hello;
+import net.trajano.ws.helloservice.SayHello;
+import net.trajano.ws.helloservice.SayHelloResponse;
+import net.trajano.ws.helloservice.impl.HelloImpl;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class SanityTest {
+	@Test
+	public void doHello() {
+		final Hello h;
+		{
+			final HelloImpl impl = new HelloImpl();
+			impl.setHelloString("ABC");
+			h = impl;
+		}
+		final SayHello parameters = new SayHello();
+		parameters.setIn("abc");
+		final SayHelloResponse sayHello = h.sayHello(parameters);
+		Assert.assertEquals("{{ABCabc", sayHello.getOut());
+	}
+
 	@Test
 	public void doNothing() {
 		Assert.assertTrue(true);
