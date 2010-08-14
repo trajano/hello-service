9cc9c61dc5ba19348d1fb405a42d208bf5c21281
Archimedes Trajano <arch@trajano.net>
2010-08-14 15:34:43 -0400

    started relocated the files to go to the CXF testers.

--- src/test/java/net/trajano/ws/helloservice/test/ConnectionTest.java
+++ src/test/java/net/trajano/ws/helloservice/test/ConnectionTest.java
@@ -1,4 +1,4 @@
-package net.trajano.ws.helloservice.itest;
+package net.trajano.ws.helloservice.test;
 
 import javax.xml.ws.BindingProvider;
 
@@ -7,7 +7,12 @@ import net.trajano.ws.helloservice.Hello;
 import net.trajano.ws.helloservice.HelloService;
 import net.trajano.ws.helloservice.SayHello;
 import net.trajano.ws.helloservice.SayHelloResponse;
+import net.trajano.ws.helloservice.impl.HelloImpl;
 
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.frontend.ServerFactoryBean;
+import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+import org.apache.cxf.test.AbstractCXFSpringTest;
 import org.junit.Test;
 
 /**
@@ -17,9 +22,27 @@ import org.junit.Test;
  * @author Archimedes Trajano <arch@trajano.net>
  * 
  */
-public class ConnectionTest {
+public class ConnectionTest extends AbstractCXFSpringTest {
+	@Test
+	public void doNothing() {
+
+	}
+
+	@Override
+	protected String[] getConfigLocations() {
+		return new String[] { "classpath:cxf-beans.xml" };
+	}
+
 	@Test
 	public void testSomething() throws Exception {
+
+		// Create an HTTP endpoint
+		final ServerFactoryBean sfb = new JaxWsServerFactoryBean();
+		sfb.setServiceClass(HelloImpl.class);
+		sfb.setAddress("http://localhost:9001/Hello");
+		// sfb.setDestinationFactory(destinationFactory)
+		final Server server = sfb.create();
+
 		final HelloService helloService = new HelloService();
 		final Hello h = helloService.getDevPort();
 		final SayHello parameters = new SayHello();
