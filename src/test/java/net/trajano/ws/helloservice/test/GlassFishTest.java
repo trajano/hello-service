94f9500cd50c18633021c5f2abdcea908cf8ffa0
Archimedes Trajano <arch@trajano.net>
2010-08-25 23:18:30 -0400

    ignored test

--- src/test/java/net/trajano/ws/helloservice/test/GlassFishTest.java
+++ src/test/java/net/trajano/ws/helloservice/test/GlassFishTest.java
@@ -8,6 +8,7 @@ import net.trajano.ws.helloservice.HelloService;
 import net.trajano.ws.schema.business.BaseType;
 import net.trajano.ws.schema.business.DerivedType;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -19,7 +20,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author Archimedes Trajano <arch@trajano.net>
  * 
  */
-// @Ignore
+@Ignore
 public class GlassFishTest {
 
 	/**
