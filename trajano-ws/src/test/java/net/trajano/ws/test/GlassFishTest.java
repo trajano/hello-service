77ee9f8a0c9f26269191eea2216a58c2d8c80b03
Archimedes Trajano <arch@trajano.net>
2010-08-30 18:32:01 -0400

    ignored test

--- trajano-ws/src/test/java/net/trajano/ws/test/GlassFishTest.java
+++ trajano-ws/src/test/java/net/trajano/ws/test/GlassFishTest.java
@@ -20,7 +20,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author Archimedes Trajano <arch@trajano.net>
  * 
  */
-//@Ignore
+@Ignore
 public class GlassFishTest {
 
 	/**
