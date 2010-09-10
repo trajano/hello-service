11b28ca755caab87cf1c5d36edc0cfbcff93975c
Archimedes Trajano <arch@trajano.net>
2010-09-09 22:57:21 -0400

    Formatted the WSDL. Tried using WEB-INF/wsdl to contain the WSDL Location instead.

--- trajano-ws/src/main/java/net/trajano/logging/SimpleService.java
+++ trajano-ws/src/main/java/net/trajano/logging/SimpleService.java
@@ -2,7 +2,7 @@ package net.trajano.logging;
 
 import javax.jws.WebService;
 
-@WebService(wsdlLocation="SimpleServiceService.wsdl")
+@WebService(wsdlLocation = "WEB-INF/wsdl/SimpleServiceService.wsdl")
 public class SimpleService {
 	public long add(long a, long b) {
 		System.out.println("adding " + a + " and " + b);
