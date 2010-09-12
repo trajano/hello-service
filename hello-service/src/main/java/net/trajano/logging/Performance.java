168eb0eb021ee7be82836f7859651a84a4d742fe
Archimedes Trajano <arch@trajano.net>
2010-09-12 11:56:35 -0400

    Relocated the PerformanceInterceptor to the logger package.

---
+++ hello-service/src/main/java/net/trajano/logging/Performance.java
@@ -0,0 +1,31 @@
+package net.trajano.logging;
+
+import java.text.MessageFormat;
+
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.InvocationContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Performance {
+	/**
+	 * Logger.
+	 */
+	private final Log log = LogFactory.getLog(this.getClass());
+
+	// private final ThreadLocal<Long> startTimeInMilli = new
+	// ThreadLocal<Long>();
+	@AroundInvoke
+	public Object log(final InvocationContext i) throws Exception {
+		final long t0 = System.currentTimeMillis();
+		try {
+			return i.proceed();
+		} finally {
+			final long t1 = System.currentTimeMillis();
+			log.info(MessageFormat.format("{0}.{1} {2}ms", i.getMethod()
+					.getDeclaringClass().getName(), i.getMethod().getName(), t1
+					- t0));
+		}
+	}
+}
---
+++ hello-service/src/main/java/net/trajano/logging/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <p>This package provides classes used for logging. This provides an example of what
+ * operations may use.</p>
+ * <p>The package does not provide a logging framework; JCL is the logging framework used.
+ * JCL was chosen as it is already used by Spring.</p>
+ * <p>This package eventually should just be moved to its own project as this would
+ * be part of a set of common interceptors.</p>
+ */
+package net.trajano.logging;
