4323adea0778a35b67b52cf15cd2eb1351d07502
Archimedes Trajano <arch@trajano.net>
2010-12-08 01:10:44 -0500

    Starting to have a new set of schemas

--- hello-service/src/test/java/net/trajano/ws/helloservice/test/SanityTest.java
+++ hello-service/src/test/java/net/trajano/ws/helloservice/test/SanityTest.java
@@ -1,5 +1,8 @@
 package net.trajano.ws.helloservice.test;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import net.trajano.ws.helloservice.SayHelloFault;
 import net.trajano.ws.helloservice.impl.Configurable;
 import net.trajano.ws.helloservice.impl.Hello;
@@ -8,6 +11,7 @@ import net.trajano.ws.schema.business.DerivedType;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.w3c.dom.Document;
 
 public class SanityTest {
 	@Test(expected = SayHelloFault.class)
@@ -42,5 +46,28 @@ public class SanityTest {
 	@Test
 	public void doNothing() {
 		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void xmlCompare() throws Exception {
+
+		final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		dbf.setCoalescing(true);
+		dbf.setIgnoringElementContentWhitespace(true);
+		dbf.setIgnoringComments(true);
+		final DocumentBuilder db = dbf.newDocumentBuilder();
+
+		final Document doc1 = db.parse(getClass().getResourceAsStream(
+				"/cxf-beans.xml"));
+		doc1.getDomConfig().setParameter("element-content-whitespace", false);
+		doc1.normalizeDocument();
+
+		final Document doc2 = db.parse(getClass().getResourceAsStream(
+				"/cxf-beans2.xml"));
+		doc2.getDomConfig().setParameter("element-content-whitespace", false);
+		doc2.normalizeDocument();
+
+		Assert.assertTrue(doc1.isEqualNode(doc2));
 	}
 }
